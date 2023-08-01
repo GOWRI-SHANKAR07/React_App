a64b370c8368da9e66d6d27b35eb968ffe2b3dfe
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-01 07:14:57 +0530

    screens updated

---
+++ screens/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Mission and The Journey So Far!</h1>
+            <p>
+                Software is created for people, and so is our mission-focused towards people-centric solutions! From creating the questful ambiance for working on tech tools to honoring the harbor that goes into making great software, we are a hub of software innovators. We always strive towards making futuristic software cost-effective and user-friendly!
+            </p>
+            <br />
+            <h1>Vision and The Road Ahead!</h1>
+            <p>
+                In the caprice game of playing with technology, being intuitive and innovative is a big deal! Knowing this piece of reality, our vision is to assist companies in embracing and tinkering with emerging technologies for the best outcomes. With the zest for creating a tech-savvy future and quest for cutting-edge technologies, we are a clan of technophilic folks, ever curious about our client needs!
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ screens/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <address>
+                Want to get on a quick call?, ring us at <br/>
+                Ohio, USA: +1 614 664 7677 <br/>
+                Singapore: +65 6652 5475 <br/>
+                Chennai, India: +91 44 4855 4277 <br/>
+                Want to visit us? Here are the Google maps directions <br/>
+                Ohio, USA <br/>
+                Singapore <br/>
+                Chennai, India <br/>
+                For Jobs enquiries, email us at jobs@spritle.com <br/>
+                For General enquiries, email us at info@spritle.com
+            </address>
+        </div>
+    )
+}
+
+export default Contact
